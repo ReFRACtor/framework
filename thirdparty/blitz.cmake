e0a1442931ac6ded76ae6e0561ed7dc87e7d5725
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-03-10 14:11:44 -0800

    Add some preliminary cmake files, starting off with third party building

---
+++ thirdparty/blitz.cmake
@@ -0,0 +1,20 @@
+set(BLITZ_NAME blitz-0.9)
+
+# Location of tar file to build
+set(BLITZ_URL ${CMAKE_CURRENT_SOURCE_DIR}/${BLITZ_NAME}.tar.gz)
+
+# This patch is described at 
+# http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=455661. This adds some
+# header files that are needed by gcc >= 4.3. These really should have
+# always been there, but the headers in gcc were a bit messy before
+set(BLITZ_PATCH ${CMAKE_CURRENT_SOURCE_DIR}/blitz++.patch)
+
+ExternalProject_Add(${BLITZ_NAME}
+    URL ${BLITZ_URL}
+    PATCH_COMMAND patch -p1 < ${BLITZ_PATCH}
+    CONFIGURE_COMMAND ./configure --prefix ${THIRDPARTY_INSTALL_PREFIX} --enable-shared --enable-shared --disable-doxygen
+        CXX=${CMAKE_CXX_COMPILER} CC=${CMAKE_C_COMPILER} F77=${CMAKE_Fortran_COMPILER}
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make 
+    INSTALL_COMMAND make install
+)
---
+++ thirdparty/lua.cmake
@@ -0,0 +1,29 @@
+set(LUA_NAME lua-5.2.2)
+
+set(LUA_URL ${CMAKE_CURRENT_SOURCE_DIR}/${LUA_NAME}.tar.gz)
+
+# This patch was taken from gentoo. It add support in the Makefile for building
+# a shared library version.
+set(LUA_PATCH ${CMAKE_CURRENT_SOURCE_DIR}/lua-make.patch)
+
+# Since these depend on other variables and have a space in them, we
+# have to define a new variable so that the values are passed to make below correctly
+set(LUA_LIBTOOL "${CMAKE_LIBTOOL} --tag=CC")
+set(LUA_CFLAGS  "${CMAKE_C_FLAGS} -DLUA_USE_LINUX")
+
+ExternalProject_Add(${LUA_NAME}
+    URL ${LUA_URL}
+    PATCH_COMMAND patch -p1 < ${LUA_PATCH}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND cd src && make gentoo_all 
+        CXX=${CMAKE_CXX_COMPILER} CC=${CMAKE_C_COMPILER} F77=${CMAKE_Fortran_COMPILER} 
+        RPATH=${THIRDPARTY_LIB_DIR}
+        LIBTOOL=${LUA_LIBTOOL}
+        CFLAGS=${LUA_CFLAGS}
+        LUA_LIBS="-lreadline -lncurses"
+        LIB_LIBS="-lm -ldl" && cd ..
+    INSTALL_COMMAND make gentoo_install INSTALL_TOP=${THIRDPARTY_INSTALL_PREFIX}
+        CXX=${CMAKE_CXX_COMPILER} CC=${CMAKE_C_COMPILER} F77=${CMAKE_Fortran_COMPILER} 
+        LIBTOOL=${LUA_LIBTOOL}
+)
