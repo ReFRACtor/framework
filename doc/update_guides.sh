cf83f3e61ac8d27936ff702ce9798d9aed0f7960
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-06-12 11:29:02 -0700

    Clean out old stuff from doc/ dir including duplicates of third party doc

--- doc/update_guides.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget -O users_guide.pdf --no-check-certificate 'https://svn.jpl.nasa.gov/xwiki/bin/export/L2+Full+Physics+Code/User%27s+Guide?format=pdf'
-wget -O developers_guide.pdf --no-check-certificate 'https://svn.jpl.nasa.gov/xwiki/bin/export/L2+Full+Physics+Code/Developer%27s+Guide?format=pdf'
