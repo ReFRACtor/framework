6edfee24fb4f94476fbb675ac3ccf784f0afdb15
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-06-16 15:10:43 -0700

    Integrate full test runs into cmake/ctest build system

---
+++ test/full/test_list.cmake
@@ -0,0 +1,20 @@
+list(APPEND FULL_TEST_LIST
+    sample
+    l2_full
+    fts
+    fts_sim
+    tccon_sounding_1
+    # These cases are very sensitive to certain machine architectures
+    # and cause spurious differences, ie retrieval is not stable
+    #tccon_sounding_2
+    #tccon_sounding_3
+    tccon_sounding_4
+    tccon_sounding_5
+    # This test uses ascii files, not really essential to test anymore
+    #oco_simple_vs_26 
+    oco2_2stream_fm
+    oco2_sounding_1
+    oco2_sounding_2
+    oco2_sounding_3
+    oco2_brdf
+)
