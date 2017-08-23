151925bbe50d0d12edfbb9fbe25c6d5a27683f0f
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-08-23 13:31:39 -0700

    Fix some unit tests that used old L1b classes to now use example class

--- lib/Implementation/level_1b_cache_test.cc
+++ lib/Implementation/level_1b_cache_test.cc
@@ -1,7 +1,6 @@
 #include "unit_test_support.h"
 #include "level_1b_cache.h"
-#include "level_1b_oco.h"
-#include "oco_sounding_id.h"
+#include "example_level_1b.h"
 #include "fp_exception.h"
 
 using namespace FullPhysics;
@@ -11,61 +10,68 @@ BOOST_FIXTURE_TEST_SUITE(level_1b_cache, GlobalFixture)
 
 BOOST_AUTO_TEST_CASE(basic)
 {
-  boost::shared_ptr<HdfFile> hfile(new HdfFile(test_data_dir() + "/oco2_L1bScND_80008a_111017225030d_spliced.h5"));
-  boost::shared_ptr<HdfSoundingId> sid(new OcoSoundingId(*hfile, "2010090900133834"));
-  Level1bOco l1b_oco = Level1bOco(hfile, sid);
-  Level1bCache l1b_cache(l1b_oco);
+    boost::shared_ptr<HdfFile> hfile(new HdfFile(test_data_dir() + "in/common/l1b_example_data.h5"));
+    ExampleLevel1b l1b_ex = ExampleLevel1b(hfile, "2014090915251774");
+    Level1bCache l1b_cache(l1b_ex);
 
-  BOOST_CHECK_EQUAL(l1b_cache.number_spectrometer(), 3);
+    BOOST_CHECK_EQUAL(l1b_cache.number_spectrometer(), 3);
 
-  BOOST_CHECK_CLOSE(l1b_cache.latitude(0).value, -72.2772216796875, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.latitude(1).value, -72.2772216796875, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.latitude(2).value, -72.2772216796875, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.latitude(0).value, -29.802030563354492, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.latitude(1).value, -29.802614212036133, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.latitude(2).value, -29.803081512451172, 1e-4);
 
-  BOOST_CHECK_CLOSE(l1b_cache.solar_zenith(0).value, 84.819931030273438, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.solar_zenith(1).value, 84.819931030273438, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.solar_zenith(2).value, 84.819931030273438, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.solar_zenith(0).value, 43.490657806396484, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.solar_zenith(1).value, 43.491470336914062, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.solar_zenith(2).value, 43.492301940917969, 1e-4);
 
-  BOOST_CHECK_CLOSE(l1b_cache.solar_azimuth(0).value, 306.58981323242188, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.solar_azimuth(1).value, 306.58981323242188, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.solar_azimuth(2).value, 306.58981323242188, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.solar_azimuth(0).value, 319.0213623046875, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.solar_azimuth(1).value, 319.02096557617188, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.solar_azimuth(2).value, 319.02029418945312, 1e-4);
 
-  BOOST_CHECK_CLOSE(l1b_cache.altitude(0).value, 0, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.altitude(1).value, 0, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.altitude(2).value, 0, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.altitude(0).value, 0, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.altitude(1).value, 0, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.altitude(2).value, 0, 1e-4);
 
-  BOOST_CHECK_CLOSE(l1b_cache.sounding_zenith(0).value, 0.083559617400169373, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.sounding_zenith(1).value, 0.083559617400169373, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.sounding_zenith(2).value, 0.083559617400169373, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.sounding_zenith(0).value, 34.251796722412109, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.sounding_zenith(1).value, 34.244930267333984, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.sounding_zenith(2).value, 34.242877960205078, 1e-4);
 
-  BOOST_CHECK_CLOSE(l1b_cache.sounding_azimuth(0).value, 216.58883666992188, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.sounding_azimuth(1).value, 216.58883666992188, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.sounding_azimuth(2).value, 216.58883666992188, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.sounding_azimuth(0).value, 138.78759765625, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.sounding_azimuth(1).value, 138.78660583496094, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.sounding_azimuth(2).value, 138.79510498046875, 1e-4);
 
-  for(int i = 0; i < 3; ++i) {
-    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(i)(0), 0.5, 1e-8);
-    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(i)(1), 0.5, 1e-8);
-    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(i)(2), 3.0595994758186862e-05, 1e-6);
-    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(i)(3), 0.0, 1e-8);
-  }
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(0)(0), 0.5, 1e-8);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(0)(1), 0.49999275803565979, 1e-8);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(0)(2), -0.0026892200112342834, 1e-6);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(0)(3), 0.0, 1e-8);
 
-  BOOST_CHECK_CLOSE(l1b_cache.relative_velocity(0).value, 12.393651008605957, 1e-4);
-  BOOST_CHECK_CLOSE(l1b_cache.time(0).pgs_time(), 558144826.33400011, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(1)(0), 0.5, 1e-8);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(1)(1), 0.4999927282333374, 1e-8);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(1)(2), -0.0026979264803230762, 1e-6);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(1)(3), 0.0, 1e-8);
 
-  BOOST_CHECK_EQUAL(l1b_cache.radiance(0).data().extent(blitz::firstDim), 1016);
-  BOOST_CHECK_CLOSE(l1b_cache.radiance(0).data()(1005), 1.4015902429283877e+19, 1e-4);
-  BOOST_CHECK_EQUAL(l1b_cache.radiance(1).data().extent(blitz::firstDim), 1016);
-  BOOST_CHECK_CLOSE(l1b_cache.radiance(1).data()(1005), 3.3393644229917409e+18, 1e-4);
-  BOOST_CHECK_EQUAL(l1b_cache.radiance(2).data().extent(blitz::firstDim), 1016);
-  BOOST_CHECK_CLOSE(l1b_cache.radiance(2).data()(1005), 8.0598957703770931e+17, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(2)(0), 0.5, 1e-8);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(2)(1), 0.49999341368675232, 1e-8);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(2)(2), -0.0025685979053378105, 1e-6);
+    BOOST_CHECK_CLOSE(l1b_cache.stokes_coefficient(2)(3), 0.0, 1e-8);
 
-  blitz::Array<double, 1> expt_coeff(10);
-  expt_coeff = 0.757452, 1.73508e-05, -2.7826e-09, 0, 0, 0, 0, 0, 0, 0;
-  BOOST_CHECK_MATRIX_CLOSE_TOL(l1b_cache.spectral_coefficient(0).value, expt_coeff, 1e-5);
-  expt_coeff = 1.58995, 3.62862e-05, -5.81593e-09, 0, 0, 0, 0, 0, 0, 0;
-  BOOST_CHECK_MATRIX_CLOSE_TOL(l1b_cache.spectral_coefficient(1).value, expt_coeff, 1e-5);
-  expt_coeff = 2.04124, 4.66365e-05, -7.38417e-09, 0, 0, 0, 0, 0, 0, 0;
-  BOOST_CHECK_MATRIX_CLOSE_TOL(l1b_cache.spectral_coefficient(2).value, expt_coeff, 1e-5);
+    BOOST_CHECK_CLOSE(l1b_cache.relative_velocity(0).value, 3350.394775390625, 1e-4);
+    BOOST_CHECK_CLOSE(l1b_cache.time(0).pgs_time(), 684429925.64504862, 1e-4);
+
+    BOOST_CHECK_EQUAL(l1b_cache.radiance(0).data().extent(blitz::firstDim), 1016);
+    BOOST_CHECK_CLOSE(l1b_cache.radiance(0).data()(1005), 6.0255898020264018e+19, 1e-4);
+    BOOST_CHECK_EQUAL(l1b_cache.radiance(1).data().extent(blitz::firstDim), 1016);
+    BOOST_CHECK_CLOSE(l1b_cache.radiance(1).data()(1005), 2.3494947203480289e+19, 1e-4);
+    BOOST_CHECK_EQUAL(l1b_cache.radiance(2).data().extent(blitz::firstDim), 1016);
+    BOOST_CHECK_CLOSE(l1b_cache.radiance(2).data()(1005), 8.0934638603730944e+18, 1e-4);
+
+    blitz::Array<double, 1> expt_coeff(10);
+    expt_coeff = 0.757632, 1.75147e-05, -2.84853e-09, 1.66737e-13, -9.85258e-17, 7.87483e-21;
+    BOOST_CHECK_MATRIX_CLOSE_TOL(l1b_cache.spectral_coefficient(0).value, expt_coeff, 1e-5);
+    expt_coeff = 1.59055, 3.64377e-05, -5.96467e-09, 1.01796e-12, -1.05804e-15, 3.64091e-19;
+    BOOST_CHECK_MATRIX_CLOSE_TOL(l1b_cache.spectral_coefficient(1).value, expt_coeff, 1e-5);
+    expt_coeff = 2.04304, 4.6962e-05, -7.69783e-09, 4.5771e-13, 1.10487e-17, -5.54788e-20;
+    BOOST_CHECK_MATRIX_CLOSE_TOL(l1b_cache.spectral_coefficient(2).value, expt_coeff, 1e-5);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
