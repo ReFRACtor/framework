aaaade6614de6fd3e9686de42eeb79ed7f9aca6b
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-08-31 16:09:44 -0700

    Fix EOF unit tests

--- lib/RegisterOutput/empirical_orthogonal_function_output_test.cc
+++ lib/RegisterOutput/empirical_orthogonal_function_output_test.cc
@@ -15,20 +15,21 @@ BOOST_AUTO_TEST_CASE(basic)
 {
   std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > > corr(3);
 
-  HdfFile hf(test_data_dir() + "l2_fixed_level_static_input.h5");
+  HdfFile hf_ils(test_data_dir() + "in/ils/ils_linear_table.h5");
+  HdfFile hf_eof(test_data_dir() + "in/eof/eof.h5");
   Array<bool, 1> flag(2);
   flag = true, false;
   Array<double, 1> coeff(2);
   coeff = 1.28695614e+04, 1.99492886e-01;
   boost::shared_ptr<IlsTableLinear> 
-    ils_tab(new IlsTableLinear(hf, 0, "A-Band", "o2"));
+    ils_tab(new IlsTableLinear(hf_ils, 0, "A-Band", "o2"));
   boost::shared_ptr<DispersionPolynomial>
     d(new DispersionPolynomial(coeff, flag, units::inv_cm, 
 			       ils_tab->band_name(), 1805, true));
   std::vector<boost::shared_ptr<Ils> > ils;
   ils.push_back(boost::shared_ptr<Ils>(new IlsConvolution(d, ils_tab)));
   boost::shared_ptr<EmpiricalOrthogonalFunction> 
-    zoff(new EmpiricalOrthogonalFunction(1.0, true, *d, hf, 0, 0, 1, ils_tab->band_name()));
+    zoff(new EmpiricalOrthogonalFunction(1.0, true, *d, hf_eof, 0, 0, 1, ils_tab->band_name()));
 
   EmpiricalOrthogonalFunctionOutput po(zoff, "o2");
   boost::shared_ptr<OutputHdf> out(new OutputHdf("empirical_orthogonal_function_output.h5", 20, 112, 5, 3));
