6ba73ac7ef4421c42aebf45b46fb022dd574bb80
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-12-19 15:23:44 -0800

    Fix some of the issues preventing compilation on Mac OS X

--- test/unit/boost_suite_name.sh
+++ test/unit/boost_suite_name.sh
@@ -2,4 +2,4 @@
 
 # Parses out test suite names from source files
 # Outputs as a list seperated by ; so that CMake interprets as a list
-grep '^[ ]*BOOST_FIXTURE_TEST_SUITE' $* | sed -r 's/.*\((\w+),.*\)/\1/g' | tr '\n' ';'
+grep '^[ ]*BOOST_FIXTURE_TEST_SUITE' $* | sed 's/.*(\([[:alnum:]_]*\),.*)/\1/' | tr '\n' ';'
--- thirdparty/luabind/luabind.cmake
+++ thirdparty/luabind/luabind.cmake
@@ -11,7 +11,7 @@ set(LUABIND_URL ${CMAKE_CURRENT_SOURCE_DIR}/luabind/luabind-Oberon00-60e576e.tar
 # 3. Don't put the version number in the library filename
 # 4. Turn off building of test files, just produce the libary
 # 5. Set the install path to the third party install path
-set(CMAKE_ARGS -DLUA_FOUND=1 -DLUA_INCLUDE_DIRS=${LUA_INCLUDE_DIR} -DLUABIND_DYNAMIC_LINK=1 -DLUABIND_APPEND_VERSION_SUFFIX=0 -DBUILD_TESTING=0 -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX})
+set(CMAKE_ARGS -DLUA_FOUND=1 -DLUA_INCLUDE_DIRS=${LUA_INCLUDE_DIR} -DLUABIND_DYNAMIC_LINK=1 -DLUABIND_APPEND_VERSION_SUFFIX=0 -DBUILD_TESTING=0 -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DLUA_LIBRARIES=${LUA_LIBRARIES})
 
 # If BOOST_ROOT has been specified for finding boost, then supply this to the third party cmake as well
 # Don't want to supply it with an empty location if the variable is not defined
@@ -22,7 +22,7 @@ endif()
 # Build and install manually to avoid dependence on bjam
 ExternalProject_Add(${LUABIND_NAME}
     URL ${LUABIND_URL}
-    CONFIGURE_COMMAND cmake ${CMAKE_ARGS} . 
+    CONFIGURE_COMMAND CXX=${CMAKE_CXX_COMPILER} CC=${CMAKE_C_COMPILER} cmake ${CMAKE_ARGS} . 
     BUILD_COMMAND make luabind
     BUILD_IN_SOURCE 1
 )
