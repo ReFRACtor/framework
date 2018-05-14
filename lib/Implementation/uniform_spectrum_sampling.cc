843270fdcbf9f7d75002eeb9fbe33cc2b401f0a4
James McDuffie <James.McDuffie@jpl.nasa.gov>
2018-05-14 14:59:00 -0700

    In UniformSpectrumSampling reject points that are too far from the points on the low resolution grid. This fixes an issue for discontinuous low resolution grids. Also fixed so that units are handled properly when they are not the same.

--- lib/Implementation/uniform_spectrum_sampling.cc
+++ lib/Implementation/uniform_spectrum_sampling.cc
@@ -13,24 +13,48 @@ SpectralDomain UniformSpectrumSampling::spectral_domain(int spec_index,
 
     DoubleWithUnit spacing = spec_spacing(spec_index);
 
-    // Determine an integral amount of spacing units to add to the beginning and end of the grid
-    DoubleWithUnit offset = round(Ils_half_width.convert_wave(spacing.units) / spacing) * spacing; 
+    // Units we input and want to output and the units we use for computing the grid
+    Unit u_out  = Lowres_grid.units();
+    Unit u_grid = spacing.units;
     
-    // Convert low res grid values to same units as spacing
-    Array<double, 1> lowres_spac_units = Lowres_grid.convert_wave(spacing.units);
+    // Convert low res grid values to same units as spacing, in sorted order
+    std::vector<double> lowres_conv;
+    BOOST_FOREACH(double v, Lowres_grid.data()) {
+        lowres_conv.push_back(DoubleWithUnit(v, Lowres_grid.units()).convert_wave(u_grid).value);
+    }
+    std::sort(lowres_conv.begin(), lowres_conv.end());
+
+    // Determine an integral amount of spacing units to add to the beginning and end of the grid
+    DoubleWithUnit offset = round(Ils_half_width.convert_wave(u_grid) / spacing) * spacing; 
 
     // Determine the bounds of the high resolution grid, account for fact the
     // low res grid points might be in a different order before conversion
-    double highres_beg = min(lowres_spac_units) - offset.value;
-    double highres_end = max(lowres_spac_units) + offset.value;
+    double highres_beg = lowres_conv[0] - offset.value;
+    double highres_end = lowres_conv[lowres_conv.size()-1] + offset.value;
 
     int nsamples = (int) floor((highres_end - highres_beg) / spacing.value) + 1;
-    Array<double, 1> highres_grid(nsamples);
+
+    std::vector<double> highres_points;
     for(int i = 0; i < nsamples; ++i) {
-        highres_grid(i) = highres_beg + i * spacing.value;
+        DoubleWithUnit hr_point(highres_beg + i * spacing.value, u_grid);
+
+        // Only use points that are within a ils_half_width of a low resolution point
+        auto lr_closest = std::lower_bound(lowres_conv.begin(), lowres_conv.end(), hr_point.value);
+        if (abs(hr_point.value - *lr_closest) <= Ils_half_width.convert_wave(u_grid).value) {
+            highres_points.push_back(hr_point.convert_wave(u_out).value);
+        }
     }
 
-    return SpectralDomain(highres_grid, spacing.units);
+    // Make sure the computed grid points are
+    // in sorted order
+    std::sort(highres_points.begin(), highres_points.end());
+
+    // Create result object
+    Array<double, 1> uniform_vals((int) highres_points.size());
+    std::copy(highres_points.begin(), highres_points.end(), uniform_vals.begin());
+    SpectralDomain uniform_grid = SpectralDomain(uniform_vals, u_out);
+
+    return uniform_grid;
 }
 
 //-----------------------------------------------------------------------
