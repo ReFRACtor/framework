8ed0916384ea8455e49b9171365d803c41f5442d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-10-17 10:39:09 -0700

    Update the name of the temporary output of a few test files, so we don't have overlap. That allows parallel unit tests to be run using CTEST_PARALLEL_LEVEL.

--- lib/RegisterOutput/temperature_met_output_test.cc
+++ lib/RegisterOutput/temperature_met_output_test.cc
@@ -10,8 +10,8 @@ BOOST_FIXTURE_TEST_SUITE(temperature_met_output, MetDataFixture)
 BOOST_AUTO_TEST_CASE(basic)
 {
   TemperatureMetOutput po(boost::shared_ptr<TemperatureMet>(new TemperatureMet(met_data, pressure, 0, true)));
-  boost::shared_ptr<OutputHdf> out(new OutputHdf("temperature_output.h5", 20, 112, 5, 3));
-  add_file_to_cleanup("temperature_output.h5");
+  boost::shared_ptr<OutputHdf> out(new OutputHdf("temperature_met_output.h5", 20, 112, 5, 3));
+  add_file_to_cleanup("temperature_met_output.h5");
   po.register_output(out);
 
   // Simple test, we just make sure that we can write output. All the
