67e3228508381e7864a73d8ec9ea585bf53aefe9
sval <Sebastian.Val@jpl.nasa.gov>
2019-02-06 10:48:53 -0800

    Adding doc, support for varying spec index, and threshold to Level1bInfo

--- lib/Interface/level_1b_info.cc
+++ lib/Interface/level_1b_info.cc
@@ -3,45 +3,55 @@
 #include <boost/geometry.hpp>
 
 #include "level_1b_info.h"
+#include "default_constant.h"
 
 
 using namespace FullPhysics;
 using namespace blitz;
 
 
-// TODO: DoubleWithUnit is probably a smarter choice here
-double Level1bInfo::obs_distance(boost::shared_ptr<Level1b> level1b_ptr, double lat, double lon) {
+DoubleWithUnit Level1bInfo::obs_distance(boost::shared_ptr<Level1b> level1b_ptr, double lat, double lon, int spec_index) {
+    DefaultConstant cs;
     Level1b* level1b = level1b_ptr.get();
-    // TODO: Add support for varying instrument index
-    DoubleWithUnit curr_lat = level1b->latitude(0);
-    DoubleWithUnit curr_lon = level1b->longitude(0);        
-    // TODO: Add earth radius someplace globally?
-    double const earth_radius = 6378.2064; // From Clarke 1866
+    DoubleWithUnit curr_lat = level1b->latitude(spec_index);
+    DoubleWithUnit curr_lon = level1b->longitude(spec_index);
     // TODO: Add typedef for spherical_point someplace globally?
     typedef boost::geometry::model::point<double, 2, boost::geometry::cs::spherical_equatorial<boost::geometry::degree>> spherical_point;
     spherical_point in_point(lon, lat);
     spherical_point curr_point(curr_lon.value, curr_lat.value);
-    // use strategy::distance::haversine to get distance (in km)
-    double seperation = boost::geometry::distance(in_point, curr_point) * earth_radius;
+    // Uses strategy::distance::haversine to get distance
+    DoubleWithUnit seperation = boost::geometry::distance(in_point, curr_point) * cs.equatorial_radius();
     return seperation;   
 }
 
-bool Level1bInfo::compare_level1b_distance(boost::shared_ptr<Level1b>& level1b_1, boost::shared_ptr<Level1b>& level1b_2, double lat, double lon) {
-    double obs1_dist = std::abs(Level1bInfo::obs_distance(level1b_1, lat, lon));
-    double obs2_dist = std::abs(Level1bInfo::obs_distance(level1b_2, lat, lon));
-    return (obs1_dist < obs2_dist);
+bool Level1bInfo::compare_level1b_distance(boost::shared_ptr<Level1b>& level1b_1, boost::shared_ptr<Level1b>& level1b_2, double lat, double lon, int spec_index) {
+    DoubleWithUnit obs1_dist = Level1bInfo::obs_distance(level1b_1, lat, lon, spec_index);
+    DoubleWithUnit obs2_dist = Level1bInfo::obs_distance(level1b_2, lat, lon, spec_index).convert(obs1_dist.units);
+    return (std::abs(obs1_dist.value) < std::abs(obs2_dist.value));
 }
 
-bool Level1bInfo::compare_level1b_time(boost::shared_ptr<Level1b>& level1b_1, boost::shared_ptr<Level1b>& level1b_2, Time search_time) {
-    // TODO: Add support for varying spec index
-    double obs1_timediff = std::abs((level1b_1->time(0) - search_time));
-    double obs2_timediff = std::abs((level1b_2->time(0) - search_time));
+bool Level1bInfo::compare_level1b_time(boost::shared_ptr<Level1b>& level1b_1, boost::shared_ptr<Level1b>& level1b_2, Time search_time, int spec_index) {
+    double obs1_timediff = std::abs((level1b_1->time(spec_index) - search_time));
+    double obs2_timediff = std::abs((level1b_2->time(spec_index) - search_time));
     return (obs1_timediff < obs2_timediff);
 }
 
-std::vector<boost::shared_ptr<Level1b>> Level1bInfo::closest_obs_n(double lat, double lon, int n) {
+std::vector<boost::shared_ptr<Level1b>> Level1bInfo::close_obs(double lat, double lon, DoubleWithUnit threshold, int spec_index) {
     std::vector<boost::shared_ptr<Level1b>> all_level1b = level1b_list();
-    auto compare_level1b_latlon = std::bind(Level1bInfo::compare_level1b_distance, std::placeholders::_1, std::placeholders::_2, lat, lon);
+
+    std::vector<boost::shared_ptr<Level1b>> close_observations = std::vector<boost::shared_ptr<Level1b>>();
+    for(auto level1b : all_level1b) {
+        DoubleWithUnit dist = Level1bInfo::obs_distance(level1b, lat, lon, spec_index).convert(threshold.units);
+        if (std::abs(threshold.value) - std::abs(dist.value) > 0) {
+            close_observations.push_back(level1b);
+        }
+    }
+    return close_observations;
+}
+
+std::vector<boost::shared_ptr<Level1b>> Level1bInfo::closest_obs_n(double lat, double lon, int n, int spec_index) {
+    std::vector<boost::shared_ptr<Level1b>> all_level1b = level1b_list();
+    auto compare_level1b_latlon = std::bind(Level1bInfo::compare_level1b_distance, std::placeholders::_1, std::placeholders::_2, lat, lon, spec_index);
     std::sort(all_level1b.begin(), all_level1b.end(), compare_level1b_latlon);
 
     std::vector<boost::shared_ptr<Level1b>> closest_observations = std::vector<boost::shared_ptr<Level1b>>();
@@ -51,15 +61,31 @@ std::vector<boost::shared_ptr<Level1b>> Level1bInfo::closest_obs_n(double lat, d
     return closest_observations;
 }
 
-boost::shared_ptr<Level1b> Level1bInfo::closest_obs(double lat, double lon) {
-    std::vector<boost::shared_ptr<Level1b>> closest = closest_obs_n(lat, lon, 1);
-    // TODO: What do we return if no obs from above? all_observation.size() == 0
-    return closest.front();    
+boost::shared_ptr<Level1b> Level1bInfo::closest_obs(double lat, double lon, int spec_index) {
+    std::vector<boost::shared_ptr<Level1b>> closest = closest_obs_n(lat, lon, 1, spec_index);
+    if (closest.size()) {
+        return closest.front();
+    }
+    return boost::shared_ptr<Level1b>();
 }
 
-std::vector<boost::shared_ptr<Level1b>> Level1bInfo::closest_obs_n(Time search_time, int n) {
+std::vector<boost::shared_ptr<Level1b>> Level1bInfo::close_obs(Time search_time, Time threshold, int spec_index) {
     std::vector<boost::shared_ptr<Level1b>> all_level1b = level1b_list();
-    auto compare_level1b_timediff = std::bind(Level1bInfo::compare_level1b_time, std::placeholders::_1, std::placeholders::_2, search_time);
+
+    std::vector<boost::shared_ptr<Level1b>> close_observations = std::vector<boost::shared_ptr<Level1b>>();
+    for(auto level1b : all_level1b) {
+        double timediff = std::abs(level1b->time(spec_index) - search_time);
+        if (std::abs(threshold.unix_time()) - timediff > 0) {
+            close_observations.push_back(level1b);
+        }
+    }
+
+    return close_observations;
+}
+
+std::vector<boost::shared_ptr<Level1b>> Level1bInfo::closest_obs_n(Time search_time, int n, int spec_index) {
+    std::vector<boost::shared_ptr<Level1b>> all_level1b = level1b_list();
+    auto compare_level1b_timediff = std::bind(Level1bInfo::compare_level1b_time, std::placeholders::_1, std::placeholders::_2, search_time, spec_index);
     std::sort(all_level1b.begin(), all_level1b.end(), compare_level1b_timediff);
 
     std::vector<boost::shared_ptr<Level1b>> closest_observations = std::vector<boost::shared_ptr<Level1b>>();
@@ -69,10 +95,12 @@ std::vector<boost::shared_ptr<Level1b>> Level1bInfo::closest_obs_n(Time search_t
     return closest_observations;
 }
 
-boost::shared_ptr<Level1b> Level1bInfo::closest_obs(Time search_time) {
-    std::vector<boost::shared_ptr<Level1b>> closest = closest_obs_n(search_time, 1);
-    // TODO: What do we return if no obs from above? all_observation.size() == 0
-    return closest.front();    
+boost::shared_ptr<Level1b> Level1bInfo::closest_obs(Time search_time, int spec_index) {
+    std::vector<boost::shared_ptr<Level1b>> closest = closest_obs_n(search_time, 1, spec_index);
+    if (closest.size()) {
+        return closest.front();
+    }
+    return boost::shared_ptr<Level1b>();
 }
 
 
--- lib/Interface/level_1b_info.h
+++ lib/Interface/level_1b_info.h
@@ -5,6 +5,7 @@
 
 #include "fp_time.h"
 #include "level_1b.h"
+#include "double_with_unit.h"
 
 
 namespace FullPhysics {
@@ -14,23 +15,69 @@ namespace FullPhysics {
 
 class Level1bInfo {
 public:
+
+//-----------------------------------------------------------------------
+/// Returns a list containing all valid level1b observations
+//-----------------------------------------------------------------------
     virtual std::vector<boost::shared_ptr<Level1b>> level1b_list() = 0;
+
+//-----------------------------------------------------------------------
+/// Returns level1b observations that are closer than the thershold
+/// to the lat/lon
+/// May be empty
+//-----------------------------------------------------------------------
+    virtual std::vector<boost::shared_ptr<Level1b>> close_obs(double lat, double lon, DoubleWithUnit threshold, int spec_index=0);
+
+
+//-----------------------------------------------------------------------
+/// Returns "n" level1b observations that are closest to the lat/lon
+/// May be empty or contain less than n observations
+//-----------------------------------------------------------------------
+    virtual std::vector<boost::shared_ptr<Level1b>> closest_obs_n(double lat, double lon, int n, int spec_index=0);
     
-    virtual std::vector<boost::shared_ptr<Level1b>> closest_obs_n(double lat, double lon, int n);
+//-----------------------------------------------------------------------
+/// Returns the level1b observation that is closest to the lat/lon
+/// May be NULL
+//-----------------------------------------------------------------------
+    virtual boost::shared_ptr<Level1b> closest_obs(double lat, double lon, int spec_index=0);
     
-    virtual boost::shared_ptr<Level1b> closest_obs(double lat, double lon);
+
+//-----------------------------------------------------------------------
+/// Returns level1b observations that are closer than the thershold
+/// to the search_time
+/// May be empty
+//-----------------------------------------------------------------------
+    virtual std::vector<boost::shared_ptr<Level1b>> close_obs(Time search_time, Time threshold, int spec_index=0);
+
+//-----------------------------------------------------------------------
+/// Returns "n" level1b observations that are closest to the search_time
+/// May be empty or contain less than n observations
+//-----------------------------------------------------------------------
+    virtual std::vector<boost::shared_ptr<Level1b>> closest_obs_n(Time search_time, int n, int spec_index=0);
+
+//-----------------------------------------------------------------------
+/// Returns the level1b observation that is closest to the search_time
+/// May be NULL
+//-----------------------------------------------------------------------
+    virtual boost::shared_ptr<Level1b> closest_obs(Time search_time, int spec_index=0);
     
-    virtual std::vector<boost::shared_ptr<Level1b>> closest_obs_n(Time search_time, int n);
-    
-    virtual boost::shared_ptr<Level1b> closest_obs(Time search_time);
-    
-    static double obs_distance(boost::shared_ptr<Level1b>, double lat, double lon);
+//-----------------------------------------------------------------------
+/// Returns the distance from given level1b observation to the lat/lon
+//-----------------------------------------------------------------------
+    static DoubleWithUnit obs_distance(boost::shared_ptr<Level1b>, double lat, double lon, int spec_index=0);
 
     virtual ~Level1bInfo() { };
 
 private:
-    static bool compare_level1b_distance(boost::shared_ptr<Level1b>& lev1b_1, boost::shared_ptr<Level1b>& lev1b_2, double lat, double lon);
-    static bool compare_level1b_time(boost::shared_ptr<Level1b>& level1b_1, boost::shared_ptr<Level1b>& level1b_2, Time search_time);
+//-----------------------------------------------------------------------
+/// Internal comparator for use in sorting level1b observations by lat/lon
+//-----------------------------------------------------------------------
+    static bool compare_level1b_distance(boost::shared_ptr<Level1b>& lev1b_1, boost::shared_ptr<Level1b>& lev1b_2, double lat, double lon, int spec_index=0);
+
+//-----------------------------------------------------------------------
+/// Internal comparator for use in sorting level1b observations by time
+//-----------------------------------------------------------------------
+    static bool compare_level1b_time(boost::shared_ptr<Level1b>& level1b_1, boost::shared_ptr<Level1b>& level1b_2, Time search_time, int spec_index=0);
 
 };
 
