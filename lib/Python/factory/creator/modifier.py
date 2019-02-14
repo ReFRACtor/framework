43071293d5d58d52d57d99824680b1fb4552c963
James McDuffie <James.McDuffie@jpl.nasa.gov>
2019-02-14 14:29:47 -0800

    Modify creator singleton modifier to replay objects created during proxied creator creation back through the creator object observer system.

--- lib/Python/factory/creator/modifier.py
+++ lib/Python/factory/creator/modifier.py
@@ -2,19 +2,28 @@ from .base import ParamIterateCreator
 from .. import param
 
 singleton_instances = {}
+singleton_captured_objects = {}
 
-def singleton(SingletonCls, storage=singleton_instances, key=None):
+def singleton(SingletonCls, instances=singleton_instances, captured_objects=singleton_captured_objects, key=None):
     """Turns any creator into a singleton that only creates an object once, storage can be overridden
     from the default with the storage keyword argument. Instances are stored by the original class's
     name by default unless a different key is supplied.
     """
+
+    storage_key = key or SingletonCls.__name__
     
     # This must be defined inside the decorator function so a fresh version is always returned
     class SingletonCreator(ParamIterateCreator):
-        def create(self, **kwargs):
-            storage_key = key or SingletonCls.__name__
 
-            singleton_value = storage.get(storage_key, None)
+        def receive(self, obj):
+
+            obj_list = captured_objects.get(storage_key, [])
+            obj_list.append(obj)
+            captured_objects[storage_key] = obj_list
+
+        def create(self, **kwargs):
+
+            singleton_value = instances.get(storage_key, None)
 
             # Defer parameter access until the wrapped creator calls for the item
             # There may be some internal order needed for accessing the parameters
@@ -30,11 +39,21 @@ def singleton(SingletonCls, storage=singleton_instances, key=None):
                 for param_name in self.param_names:
                     param_values[param_name] = param_wrapper(param_name)
 
+                # Record objects created from the proxied creator for replaying during caching
+                self.register_to_receive(object)
+
                 creator = SingletonCls(param_values, common_store=self.common_store)
-
                 singleton_value = creator.create(**kwargs)
+                instances[storage_key] = singleton_value
 
-                storage[storage_key] = singleton_value
+                # Unregister to keep from capturing objects outside the scope of the proxied creator
+                self.deregister_to_receive(object)
+            else:
+                # Emit captured objects from first storage, replaying what happened
+                # when the proxied creator was actually created
+                obj_list = captured_objects.get(storage_key, [])
+                for obj in obj_list:
+                    self._dispatch(obj)
 
             return singleton_value
 
