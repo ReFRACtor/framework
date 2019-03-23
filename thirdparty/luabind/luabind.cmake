277957156327348934fc79fe3a6383f38c76591d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-03-23 11:55:52 -0700

    Need to turn off optimization in build of luabind for gcc 7.3

--- thirdparty/luabind/luabind.cmake
+++ thirdparty/luabind/luabind.cmake
@@ -18,8 +18,19 @@ set(CMAKE_ARGS -DLUA_FOUND=1 -DLUA_INCLUDE_DIRS=${LUA_INCLUDE_DIR} -DLUABIND_DYN
 # If BOOST_ROOT has been specified for finding boost, then supply this to the third party cmake as well
 # Don't want to supply it with an empty location if the variable is not defined
 if(BOOST_ROOT)
-    list(APPEND CMAKE_ARGS -DBOOST_ROOT=${BOOST_ROOT})
-endif()
+  list(APPEND CMAKE_ARGS -DBOOST_ROOT=${BOOST_ROOT})
+endif(BOOST_ROOT)
+
+# For GCC 7.3, luabind has a segmentation fault when compiled optimized. This
+# is probably some bug in luabind that we could fix, but 1) luabind isn't
+# really updated anymore and 2) we will move lua out of ReFactor in favor of
+# python. So it isn't worth trying to fix. Just turn off optimization for newer
+# versions of the compiler. Since this is just used by lua configuration, this
+# shouldn't have much of an impact on over all performance
+set(LUABIND_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+if(CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5.0)
+  set(LUABIND_CMAKE_CXX_FLAGS ${LUABIND_CMAKE_CXX_FLAGS} -O0)
+endif(CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5.0)
 
 # Build and install manually to avoid dependence on bjam
 ExternalProject_Add(${LUABIND_NAME}
