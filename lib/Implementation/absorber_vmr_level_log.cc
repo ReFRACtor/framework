30332bf529f3aadacc2154bc2192f86185317a70
sval <Sebastian.Val@jpl.nasa.gov>
2019-04-08 10:55:29 -0700

    Making AbsorberVmrLevelLog subclass of AbsorberVmrLevel with only mapping, naming, and print differences

--- lib/Implementation/absorber_vmr_level_log.cc
+++ lib/Implementation/absorber_vmr_level_log.cc
@@ -16,30 +16,8 @@ AbsorberVmrLevelLog::AbsorberVmrLevelLog(const boost::shared_ptr<Pressure>& Pres
                                          const blitz::Array<double, 1>& Vmr,
                                          const blitz::Array<bool, 1>& Vmr_flag,
                                          const std::string& Gas_name)
-: AbsorberVmrImpBase(Gas_name, Vmr, Vmr_flag, Press, false, 0, boost::make_shared<MappingLog>())
+: AbsorberVmrLevel(Press, Vmr, Vmr_flag, Gas_name, boost::make_shared<MappingLog>())
 {
-}
-
-boost::shared_ptr<AbsorberVmr> AbsorberVmrLevelLog::clone(const boost::shared_ptr<Pressure>& Press) const
-{
-    return boost::shared_ptr<AbsorberVmr>(new AbsorberVmrLevelLog(Press, coefficient().value(), used_flag, gas_name()));
-}
-
-void AbsorberVmrLevelLog::calc_vmr() const
-{
-    std::vector<AutoDerivative<double> > plist;
-    std::vector<AutoDerivative<double> > vmrlist;
-
-    for(int i = 0; i < press->pressure_grid().rows(); ++i) {
-        vmrlist.push_back(mapping->apply_element(coeff(i)));
-        plist.push_back(press->pressure_grid()(i).value);
-    }
-
-    typedef LinearInterpolate<AutoDerivative<double>, AutoDerivative<double> > lin_type;
-
-    boost::shared_ptr<lin_type> lin (new lin_type(plist.begin(), plist.end(), vmrlist.begin()));
-
-    vmr = boost::bind(&lin_type::operator(), lin, _1);
 }
 
 void AbsorberVmrLevelLog::print(std::ostream& Os) const
@@ -48,7 +26,7 @@ void AbsorberVmrLevelLog::print(std::ostream& Os) const
     Os << "AbsorberVmrLevelLog:\n"
        << "  Gas name:       " << gas_name() << "\n"
        << "  Coefficient:\n";
-    opad << coeff.value() << "\n";
+    opad << coefficient_unmapped().value() << "\n";
     opad.strict_sync();
     Os << "  Retrieval Flag:\n";
     opad << used_flag << "\n";
