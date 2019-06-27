105a7b74fff34c6934b2cb190fd9d34b3398a06a
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-06-26 21:31:13 -0700

    Clean up unit test

--- lib/Python/solver_test.py
+++ lib/Python/solver_test.py
@@ -5,17 +5,19 @@ def test_gsl_bard():
     nlls = rf.BardNLLSProblem()
     nlls.parameters = [1.0,1.0,1.0]
     solver =rf.NLLSSolverGSL(nlls, 100, 1e-5, 1e-5, 1e-5, False)
-    assert nlls.cost >  0.001
+    assert nlls.cost >  0.01
     solver.solve()
-    assert nlls.cost >  0.001
-    # This illustrated a memory leak message from SWIG that we will want
-    # to fix.
-    print(solver.accepted_points)
+    assert nlls.cost <  0.01
+    # This illustrated a previous memory leak message from SWIG 
+    # - this has been fixed now.
+    pt = solver.accepted_points
+    assert len(pt) > 3
 
 def test_fm(config_forward_model):
     '''This illustrates our previous lack of support of 
     boost::optional<blitz::Range> - fixed now'''
-    print(config_forward_model.stacked_pixel_range(0))
+    assert config_forward_model.stacked_pixel_range(0).first() == 0
+    assert config_forward_model.stacked_pixel_range(0).last() == 835
 
 def test_to_vector():
     '''This illustrates a problem with to_vector.'''
