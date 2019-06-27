414dcbcbdf9849f085a425148f31bfeead75530d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-06-26 21:24:02 -0700

    Fixes #60. Turns outs the to_vector problem mentioned in #60 is for another unrelated problem, we didn't break this with the swig updates. We'll open another issue for that and track it separately.

--- lib/Python/solver_test.py
+++ lib/Python/solver_test.py
@@ -13,7 +13,8 @@ def test_gsl_bard():
     print(solver.accepted_points)
 
 def test_fm(config_forward_model):
-    '''This illustrates our lack of support of boost::optional<blitz::Range>'''
+    '''This illustrates our previous lack of support of 
+    boost::optional<blitz::Range> - fixed now'''
     print(config_forward_model.stacked_pixel_range(0))
 
 def test_to_vector():
@@ -21,4 +22,22 @@ def test_to_vector():
     a = rf.ArrayAd_double_1(2,3)
     a[0] = rf.AutoDerivativeDouble(1)
     a[1] = rf.AutoDerivativeDouble(2)
-    assert a.to_vector()[0].value == a[0].value
+    # This gives an error, because the vector in a.to_vector() gets freeded
+    # while we still have the [0] in python - leading to a pointer to free
+    # memory. Not a general problem, most of our vectors are native types
+    # (e.g., double) or boost::shared_ptr which doesn't have the same issue.
+    # A std::vector<AutoDerivative<double> > is a special case that causes
+    # problems.
+    #
+    # Run with "valgrind --log-file=temp.log --malloc-fill=0xff
+    # --free-fill=0xff `which python` `which pytest`" to force freeded data
+    # to NaN so we can fully illustrate this problem
+ 
+    if(False):
+        assert a.to_vector()[0].value == a[0].value
+
+    # This succeeds, because the vector is still around in "t" while the
+    # value is being used.
+    t = a.to_vector()
+    assert t[0].value == a[0].value
+    
