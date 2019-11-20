b74ec2d6158c9f9d151e7a827918521941931bdc
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-11-20 09:56:31 -0800

    Have ModelMeasureStandard actually using multiple forward models. Not well tested yet, turns out we don't have an unit tests in framework. We'll test this in one of the other repositories using python.

--- lib/Implementation/model_measure_standard.cc
+++ lib/Implementation/model_measure_standard.cc
@@ -5,6 +5,26 @@ using namespace FullPhysics;
 using namespace blitz;
 
 
+// Function to append a second array at the bottom of an existing
+// array.
+
+template<class T> void append_array(blitz::Array<T, 1>& A, const blitz::Array<T, 1>& A_to_append)
+{
+  Range r_to(A.rows(), A.rows() + A_to_append.rows());
+  A.resizeAndPreserve(A.rows() + A_to_append.rows());
+  A(r_to) = A_to_append(Range::all());
+}
+
+template<class T> void append_array(blitz::Array<T, 2>& A, const blitz::Array<T, 2>& A_to_append)
+{
+  if(A.cols() != A_to_append.cols())
+    throw Exception("Matrices need to have the same number of columns");
+  Range r_to(A.rows(), A.rows() + A_to_append.rows());
+  A.resizeAndPreserve(A.rows() + A_to_append.rows(), A.cols());
+  A(r_to, Range::all()) = A_to_append(Range::all(), Range::all());
+}
+
+
 ModelMeasureStandard::ModelMeasureStandard
 (const boost::shared_ptr<ForwardModel>& forward_model, 
  const boost::shared_ptr<Observation>& observation, 
@@ -28,9 +48,16 @@ ModelMeasureStandard::ModelMeasureStandard
     throw Exception("Need to have at least one forward model.");
   if(forward_model.size() != observation.size())
     throw Exception("forward_model and observation vectors need to be the same size");
-  set_measurement(obs[0]->radiance_all().spectral_range().data(),
-		  Array<double, 1>(sqr(obs[0]->radiance_all().spectral_range().uncertainty())));
+  Array<double, 1> a1(obs[0]->radiance_all().spectral_range().data());
+  Array<double, 1> a2(sqr(obs[0]->radiance_all().spectral_range().uncertainty()));
   meas_units = obs[0]->radiance_all().spectral_range().units();
+  for(int i = 1; i < (int) obs.size(); ++i) {
+    SpectralRange s1 = obs[i]->radiance_all().spectral_range().convert(meas_units);
+    append_array(a1, s1.data());
+    Array<double, 1> a3(sqr(s1.uncertainty()));
+    append_array(a2, a3);
+  }
+  set_measurement(a1, a2);
 }
 
 
@@ -64,13 +91,26 @@ void ModelMeasureStandard::radiance_from_fm()
 {
   assert_parameter_set_correctly();
 
+  Array<double, 1> temp_msrmnt(obs[0]->radiance_all().spectral_range().data());
+  Spectrum rad_spec = fm[0]->radiance_all(false);
+  SpectralRange rad_mod = rad_spec.spectral_range().convert(meas_units);
+  M.reference(rad_mod.data_ad().value());
+  K.reference(rad_mod.data_ad().jacobian());
+  for(int i = 1; i < (int) obs.size(); ++i) {
+    SpectralRange s1 = obs[i]->radiance_all().spectral_range().convert(meas_units);
+    append_array(temp_msrmnt, s1.data());
+    Spectrum s2 = fm[i]->radiance_all(false);
+    SpectralRange s3 = s2.spectral_range().convert(meas_units);
+    append_array(M, s3.data_ad().value());
+    append_array(K, s3.data_ad().jacobian());
+  }
+
   //  TEMPORARY
   //
   //  There should be no need for this error check block.
   //  The problem is that with our current model code 
   //  measurement may change.
   //
-  Array<double, 1> temp_msrmnt(obs[0]->radiance_all().spectral_range().data());
   if(msrmnt.rows() != temp_msrmnt.rows())
     throw Exception("Measurement has changed during the retrieval. :( ");
   double msrmnt_L1_norm = sum(abs(msrmnt));
@@ -79,23 +119,16 @@ void ModelMeasureStandard::radiance_from_fm()
   if( sum(abs(temp_msrmnt-msrmnt))/msrmnt_L1_norm > 0.0000001 )
     throw Exception("Measurement has changed during the retrieval. :( ");
 
-  Spectrum rad_spec = fm[0]->radiance_all(false);
-  SpectralRange rad_mod = rad_spec.spectral_range().convert(meas_units);
-  M.reference(rad_mod.data_ad().value());
-  assert_model_correct(M);
-  M.makeUnique();
-
-  K.reference(rad_mod.data_ad().jacobian());
-  assert_jacobian_correct(K);
-
   //  TEMPORARY
   //
   // Should go away after we end support for 
   // fixed pressure level grid.
   vanishing_params_update();
 
+  assert_model_correct(M);
+  M.makeUnique();
+  assert_jacobian_correct(K);
   K.makeUnique();
-
 }
 
 
