38abb7662d5b3035ea13bf673665f0869852f978
sval <Sebastian.Val@jpl.nasa.gov>
2020-01-08 10:30:55 -0800

    Adding WIP on OSS FM

--- lib/Implementation/oss_forward_model.h
+++ lib/Implementation/oss_forward_model.h
@@ -4,6 +4,8 @@
 #include "state_vector.h"
 #include "rt_atmosphere.h"
 
+#include <string>
+
 namespace FullPhysics {
 /****************************************************************//**
   This a forward model class that wraps the AER OSS Forward Model
@@ -35,9 +37,13 @@ class OssForwardModel : public ForwardModel {
 public:
     OssForwardModel(const boost::shared_ptr<RtAtmosphere>& Atm, const std::string& Sel_file,
         const std::string& Od_file, const std::string& Sol_file, const std::string& Fix_file,
-        const std::string& Fix_file2, const std::string& Ch_sel_file) :
-          atmosphere(Atm), sel_file(Sel_file), od_file(Od_file), sol_file(Sol_file),
-          fix_file(Fix_file), fix_file2(Fix_file2), ch_sel_file(Ch_sel_file){} ;
+        const std::string& Ch_sel_file, int Max_chans = 20000) : atmosphere(Atm),
+          sel_file(Sel_file), sel_file_sz(Sel_file.length()),
+          od_file(Od_file), od_file_sz(Od_file.length()),
+          sol_file(Sol_file), sol_file_sz(Sol_file.length()),
+          fix_file(Fix_file), fix_file_sz(Fix_file.length()),max_chans(Max_chans),
+          ch_sel_file(Ch_sel_file), ch_sel_file_sz(Ch_sel_file.length()),
+          max_chans(Max_chans) {} ;
     virtual ~OssForwardModel() {}
     virtual void setup_grid() {
       /*
@@ -46,7 +52,6 @@ public:
      od_file, od_file_sz,
      sol_file, sol_file_sz,
      fix_file, fix_file_sz,
-     fix_file2, fix_file2_sz,
      chSel_file, chSel_file_sz,
      nlevu, n_SfGrd, minExtCld,
      nchanOSS, WvnOSS, mxchan);
@@ -59,7 +64,6 @@ public:
       - od_file = file name of optical property lookup table
       - sol_file = file name of solar radiance*
       - fix_file = file name of default profiles for variable gases
-      - fix_file2 = file name of OSS data related to od_file (temporary)
       - chSelFile = file name of list(s) of channel subsets†
       - nlevu = number of vertical levels of state vector
       - nsf = number of surface grid points
@@ -71,16 +75,24 @@ public:
       */
       int nInMol = 11;
       int lenG = 10;
-      char* nameGas[] = "H2O       CO2       O3        N2O       CO        CH4       O2        NH3       CCL4      F11       F12       ";
+      char nameGas[110] = "H2O       CO2       O3        N2O       CO        CH4       O2        NH3       CCL4      F11       F12       ";
       int nInJac = 2;
       int lenJ = 2;
-      char* nameJacob[] = "H2O       NH3       i";
+      char nameJacob[30] = "H2O       NH3       i";
       int nlevu = 65;
       int n_SfGrd = 501;
       float minExtCld = 999;
       int nchanOSS;
-      float WvnOSS;
-      const int mxchan;
+      float WvnOSS[max_chans];
+
+      cppinitwrapper(nInMol, lenG, nameGas, nInJac, lenJ, nameJacob,
+           sel_file.c_str(), sel_file_sz,
+           od_file.c_str(), od_file_sz,
+           sol_file.c_str(), sol_file_sz,
+           fix_file.c_str(), fix_file_sz,
+           ch_sel_file.c_str(), ch_sel_file_sz,
+           nlevu, n_SfGrd, minExtCld,
+           nchanOSS, WvnOSS, max_chans);
 
     }
     virtual int num_channels() const { return 1; }
@@ -136,17 +148,121 @@ public:
       - xkCldlnPres = cloud center log pressure Jacobians  (W m−2 str−1 cm−1)
       - xkCldlnExt = cloud peak log extinction Jacobians  (W m−2 str−1 cm−1)
       */
+
+      /*
+(lldb) p nlevu
+(int) $0 = 65
+
+(lldb) p ngas
+(int) $1 = 11
+
+tape5.nc/Pressure
+(lldb) p Pin
+(float *) $2 = 0x0000000100c530c0
+(lldb) x/20f Pin
+0x100c530c0: 1009.40991
+0x100c530c4: 1000
+0x100c530c8: 908.513977
+0x100c530cc: 825.401978
+0x100c530d0: 749.893005
+0x100c530d4: 681.291015
+0x100c530d8: 618.966003
+0x100c530dc: 562.34198
+0x100c530e0: 510.89801
+0x100c530e4: 464.160004
+0x100c530e8: 421.697998
+0x100c530ec: 383.117004
+0x100c530f0: 348.069
+0x100c530f4: 316.22699
+0x100c530f8: 287.298004
+0x100c530fc: 261.015991
+0x100c53100: 237.136993
+0x100c53104: 215.444
+0x100c53108: 195.735001
+0x100c5310c: 177.828995
+
+tape5.nc/Temperature
+(lldb) p Temp
+(float *) $3 = 0x0000000100c53200
+
+tape5.nc/SkinTemperature
+(lldb) p Tskin
+(float) $4 = 310
+
+tape5.nc/vmrGas
+(lldb) p vmrGas
+(float *) $5 = 0x000000010104f600
+
+(lldb) p n_SfGrd
+(int) $6 = 501
+
+tape5.nc/Emissivity
+(lldb) p emis
+(float *) $7 = 0x000000010100d000
+
+tape5.nc/Reflectivity
+(lldb) p refl
+(float *) $8 = 0x0000000101050200
+
+(lldb) p scaleCld
+(float) $9 = 0
+
+(lldb) p presCld
+(float) $10 = 0
+
+(lldb) p nCld
+(int) $11 = 2
+
+(lldb) p extCld
+(float *) $12 = 0x0000000100c531d0
+
+tape5.nc/SurfaceGrid
+(lldb) p SfGrd
+(float *) $13 = 0x0000000101001800
+
+(lldb) p gridCld
+(float *) $14 = 0x0000000100c53080
+
+(lldb) p ang
+(float) $15 = 1.45646667
+
+(lldb) p sunang
+(float) $16 = 90
+
+tape5.nc/Latitude
+(lldb) p lat
+(float) $17 = 45
+
+(lldb) p altSfc
+(float) $18 = 0.0000639999998
+
+(lldb) p lambertian
+(int) $19 = 1
+
+(lldb) p nInJac
+(int) $20 = 2
+
+(lldb) p nchanOSS
+(int) $21 = 3951
+
+       */
       return Spectrum();
     }
     virtual void print(std::ostream& Os) const { Os << "OssForwardModel"; }
 private:
     boost::shared_ptr<RtAtmosphere> atmosphere;
     std::string sel_file;
+    int sel_file_sz;
     std::string od_file;
+    int od_file_sz;
     std::string sol_file;
+    int sol_file_sz;
     std::string fix_file;
-    std::string fix_file2;
+    int fix_file_sz;
     std::string ch_sel_file;
+    int ch_sel_file_sz;
+
+    const int max_chans;
 };
 }
 #endif
--- lib/Implementation/oss_forward_model_test.cc
+++ lib/Implementation/oss_forward_model_test.cc
@@ -8,7 +8,13 @@ BOOST_FIXTURE_TEST_SUITE(oss_forward_constant, AtmosphereFixture)
 
 BOOST_AUTO_TEST_CASE(radiance)
 {
-  OssForwardModel fm(atm);
+  // TODO: Add cmake variable for OSS test data dir
+  std::string sel_file = "/export/refractor_data/OSS/aura-tes-B1B2-unapod-loc-clear-23V-M12.4-v1.0.train.sel";
+  std::string od_file = "/export/refractor_data/OSS/aura-tes-B1B2-unapod-loc-clear-23V-M12.4-v1.0.train.lut";
+  std::string sol_file = "/export/refractor_data/OSS//newkur.dat";
+  std::string fix_file = "/export/refractor_data/OSS/default.dat";
+  std::string ch_sel_file = NULL;
+  OssForwardModel fm(atm, sel_file, od_file, sol_file, fix_file, ch_sel_file);
   fm.setup_grid();
   // TODO: Add checks
 }
