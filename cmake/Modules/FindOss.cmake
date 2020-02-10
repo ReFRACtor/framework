8504b37f7775a55bab78752a727bbd70b6e3dffa
sval <Sebastian.Val@jpl.nasa.gov>
2020-02-10 15:48:20 -0800

    Adding CMake variables and C++ functions for specifying and looking up OSS directories

--- cmake/Modules/FindOss.cmake
+++ cmake/Modules/FindOss.cmake
@@ -11,32 +11,32 @@
 #    GFORTRAN_LIBRARY_DIR will be set to the directory that contains libgfortran
 FIND_PATH(_OSS_INCLUDE_DIR oss_ir_module.mod
   HINTS ${OSS_INCLUDE_DIR} $ENV{OSS_DIR} ${OSS_DIR}
-  PATH_SUFFIXES include
+  PATH_SUFFIXES include include/darwin_x86_64 include/linux_x86_64
 )
 
 
 FIND_LIBRARY(_OSS_LIBRARY
   NAMES oss
   HINTS ${OSS_LIBRARY_DIR} $ENV{OSS_DIR} ${OSS_DIR}
-  PATH_SUFFIXES lib64 lib
+  PATH_SUFFIXES lib64 lib lib/darwin_x86_64 lib/linux_x86_64
 )
 
 FIND_LIBRARY(_OSS_FORTRAN_LIBRARY
   NAMES foss
   HINTS ${OSS_LIBRARY_DIR} $ENV{OSS_DIR} ${OSS_DIR}
-  PATH_SUFFIXES lib64 lib
+  PATH_SUFFIXES lib64 lib lib/darwin_x86_64 lib/linux_x86_64
 )
 
 FIND_LIBRARY(_OSS_C_LIBRARY
   NAMES coss
   HINTS ${OSS_LIBRARY_DIR} $ENV{OSS_DIR} ${OSS_DIR}
-  PATH_SUFFIXES lib64 lib
+  PATH_SUFFIXES lib64 lib lib/darwin_x86_64 lib/linux_x86_64
 )
 
 FIND_LIBRARY(_GFORTRAN_LIBRARY
   NAMES gfortran
   HINTS {GFORTRAN_LIBRARY_DIR} ${GFORTRAN_DIR} $ENV{GFORTRAN_DIR}
-  PATH_SUFFIXES lib64 lib
+  PATH_SUFFIXES lib64 lib lib/darwin_x86_64 lib/linux_x86_64
 
 )
 
