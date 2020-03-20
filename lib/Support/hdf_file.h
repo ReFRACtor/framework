eff5e8f6f13b50a5790911f14b5ca31c226a80b6
sval <Sebastian.Val@jpl.nasa.gov>
2020-03-20 11:41:49 -0700

    Adding missing #if change for HDF5 version detect logic

--- lib/Support/hdf_file.h
+++ lib/Support/hdf_file.h
@@ -171,7 +171,7 @@ private:
   H5::Attribute create_attribute(const std::string& Aname, 
 	 const H5::DataSpace& Ds, const H5::DataType& P);
   bool is_group(const std::string& Objname) const;
-#if H5_VERS_MAJOR == 1 && H5_VERS_MINOR >= 10 && H5_VERS_RELEASE >= 1
+#if (H5_VERS_MAJOR == 1 && H5_VERS_MINOR == 10 && H5_VERS_RELEASE >= 1) || (H5_VERS_MAJOR == 1 && H5_VERS_MINOR >= 11)
   bool is_present(const std::string& Objname, 
 		  const H5::H5Location& Parent) const;
 #else
