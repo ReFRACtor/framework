e6509ec99ed12b4b540cd36dc0a3c33c8ec6afdc
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2020-06-23 20:34:25 -0700

    Forgot add file to git in last checkin

---
+++ lib/Support/named_spectrum.cc
@@ -0,0 +1,16 @@
+#include "named_spectrum.h"
+#include "fp_serialize_support.h"
+using namespace FullPhysics;
+
+#ifdef FP_HAVE_BOOST_SERIALIZATION
+template<class Archive>
+void NamedSpectrum::serialize(Archive& ar,
+			      const unsigned int UNUSED(version))
+{
+  ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Spectrum)
+    & FP_NVP_(name) & FP_NVP_(index);
+}
+
+FP_IMPLEMENT(NamedSpectrum);
+#endif
+
