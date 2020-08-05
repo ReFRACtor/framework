425cd2ac94800af5eb572b2b444d286f761d6e1e
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2020-08-05 16:40:28 -0700

    Change ChapmanBoaRT to use new CalculationCache.

--- lib/RadiativeTransfer/chapman_boa_rt.h
+++ lib/RadiativeTransfer/chapman_boa_rt.h
@@ -1,19 +1,31 @@
 #ifndef CHAPMAN_BOA_RT_H
 #define CHAPMAN_BOA_RT_H
-
 #include "radiative_transfer.h"
 #include "atmosphere_standard.h"
 #include "chapman_boa.h"
 #include "spectral_bound.h"
+#include "calculation_cache.h"
 
 namespace FullPhysics {
+class ChapmanBoaRT;
 
+class ChapmanBoaRTCache : public CalculationCacheIndexed<ChapmanBoaRT>
+{
+public:
+  ChapmanBoaRTCache() {}
+  virtual ~ChapmanBoaRTCache() {}
+  void fill_cache(const ChapmanBoaRT& C, int Spec_index);
+  boost::shared_ptr<ChapmanBOA> chapman_boa;
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version);
+};
+  
 /****************************************************************//**
 *******************************************************************/
 
-class ChapmanBoaRT : public RadiativeTransfer,
-		     public Observer<RtAtmosphere>,
-                     public boost::noncopyable  {
+class ChapmanBoaRT : public RadiativeTransfer {
 public:
   ChapmanBoaRT(const boost::shared_ptr<AtmosphereStandard>& Atm,
   	       const blitz::Array<double, 1>& Sza);
@@ -23,11 +35,6 @@ public:
 	       const SpectralBound& Spec_bound);
 
   virtual ~ChapmanBoaRT() {}
-
-  //-----------------------------------------------------------------------
-  /// Regenerate chapman factors when Atmosphere changes
-  //-----------------------------------------------------------------------
-  void notify_update(const RtAtmosphere& UNUSED(updated_atm)) { chapman_cache_stale = true; }
 
   virtual int number_stokes() const { return 1; }
 
@@ -51,20 +58,12 @@ public:
   virtual std::vector<boost::shared_ptr<GenericObject> >
   subobject_list() const
   { std::vector<boost::shared_ptr<GenericObject> > res;
-    BOOST_FOREACH(auto i, chapman_boa)
-      res.push_back(i);
     res.push_back(atm);
     return res;
   }
 private:
-  /// Computes Chapman factors on demand
-  void compute_chapman_factors(const int Spec_idx) const;
-
-  /// Handles flagging whether or not chapman factors need to be regenerated
-  mutable blitz::Array<bool, 1> chapman_cache_stale;
-
-  /// Chapman BOA geometry class
-  mutable std::vector<boost::shared_ptr<ChapmanBOA> > chapman_boa;
+  friend ChapmanBoaRTCache;
+  mutable std::vector<ChapmanBoaRTCache> cache;
 
   /// Window range for use when picking refraction method
   SpectralBound spec_bound;
@@ -78,10 +77,6 @@ private:
   friend class boost::serialization::access;
   template<class Archive>
   void serialize(Archive & ar, const unsigned int version);
-  template<class Archive>
-  void save(Archive & ar, const unsigned int version) const;
-  template<class Archive>
-  void load(Archive & ar, const unsigned int version);
 };
 }
 
