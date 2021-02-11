4bf051024d8bf99517446ae3482891564996fd29
James McDuffie <James.McDuffie@jpl.nasa.gov>
2021-02-10 16:48:51 -0800

    Modify PCA RT unit test by moving range of values tested to a portion that is away from the band edge. Adjust tolerances based on offline acceptability investigation.

--- lib/RadiativeTransfer/pca_rt_test.cc
+++ lib/RadiativeTransfer/pca_rt_test.cc
@@ -12,6 +12,8 @@ using namespace blitz;
 
 void test_pca_rt(const boost::shared_ptr<AtmosphereStandard>& atm, const SpectralDomain& full_grid)
 {
+    bool debug_output = false;
+
     int num_streams = 8;
     int num_moments = 2 * num_streams;
     bool pure_nadir = false;
@@ -41,7 +43,17 @@ void test_pca_rt(const boost::shared_ptr<AtmosphereStandard>& atm, const Spectra
                   sza, zen, azm,
                   num_streams, num_moments);
 
-    SpectralDomain test_grid( full_grid.data()(Range(0, 999)), full_grid.units() );
+    // Sample the band away from the band continuum edge
+    int beg_pca_range = 3000;
+    int end_pca_range = 3999;
+    if (full_grid.data().rows() <= end_pca_range) {
+        Exception err;
+        err << "PCA test range " << beg_pca_range << ", " << end_pca_range << " "
+            << "exceeds side of full grid: " << full_grid.data().rows();
+        throw err;
+    }
+
+    SpectralDomain test_grid( full_grid.data()(Range(beg_pca_range, end_pca_range)), full_grid.units() );
 
     // Use LIDORT for comparison
     LidortRt lidort_rt (atm, stokes_coefs, sza, zen, azm, pure_nadir,
@@ -53,31 +65,64 @@ void test_pca_rt(const boost::shared_ptr<AtmosphereStandard>& atm, const Spectra
 
     blitz::Array<double, 2> lidort_stokes = lidort_rt.stokes(test_grid, 0);
 
-    BOOST_CHECK_MATRIX_CLOSE_TOL(lidort_stokes(r_all, 0), pca_stokes(r_all, 0), 1e-6);
+    BOOST_CHECK_MATRIX_CLOSE_TOL(lidort_stokes(r_all, 0), pca_stokes(r_all, 0), 2e-5);
 
     // Radiance and jacobian
     ArrayAd<double, 2> pca_stokes_and_jac = pca_rt.stokes_and_jacobian(test_grid, 0);
 
     ArrayAd<double, 2> lidort_stokes_and_jac = lidort_rt.stokes_and_jacobian(test_grid, 0);
 
-    BOOST_CHECK_MATRIX_CLOSE_TOL(lidort_stokes_and_jac.value()(r_all, 0), pca_stokes_and_jac.value()(r_all, 0), 1e-6);
+    if(debug_output) {
+        std::ofstream outfile_lid("pca_rt_test-lid_refl.out");
+        outfile_lid << std::scientific
+                    << std::setprecision(20)
+                    << "# lidort refl" << std::endl
+                    << lidort_stokes_and_jac.value()(r_all, 0) << std::endl;
+        outfile_lid.close();
+        std::ofstream outfile_pca("pca_rt_test-pca_refl.out");
+        outfile_pca << std::scientific
+                    << std::setprecision(20)
+                    << "# pca refl" << std::endl
+                    << pca_stokes_and_jac.value()(r_all, 0) << std::endl;
+        outfile_pca.close();
+    }
+
+    BOOST_CHECK_MATRIX_CLOSE_TOL(lidort_stokes_and_jac.value()(r_all, 0), pca_stokes_and_jac.value()(r_all, 0), 2e-5);
 
     // Compare the RMS difference because the PCA jacobians are more of an approximation than the radiances
     // But that is okay since they are just used to drive the retrieval
     Array<double, 2> lid_jac = lidort_stokes_and_jac.jacobian()(r_all, 0, r_all);
     Array<double, 2> pca_jac = pca_stokes_and_jac.jacobian()(r_all, 0, r_all);
 
+    if(debug_output) {
+        std::ofstream outfile_lid("pca_rt_test-lid_jac.out");
+        outfile_lid << std::scientific
+                    << std::setprecision(20)
+                    << "# lidort jac" << std::endl
+                    << lid_jac << std::endl;
+        outfile_lid.close();
+        std::ofstream outfile_pca("pca_rt_test-pca_jac.out");
+        outfile_pca << std::scientific
+                    << std::setprecision(20)
+                    << "# pca jac" << std::endl
+                    << pca_jac << std::endl;
+        outfile_pca.close();
+    }
+
     firstIndex i1; secondIndex i2;
     Array<double, 2> jac_sqr_diff( sqr(lid_jac(i1, i2) - pca_jac(i1, i2)) );
     double rms_avg = 0;
     for(int jac_idx = 0; jac_idx < jac_sqr_diff.cols(); jac_idx++) {
         double rms = sqrt(mean(jac_sqr_diff(r_all, jac_idx)));
-        BOOST_CHECK(rms < 0.7);
+        BOOST_CHECK(rms < 0.2);
+        std::cerr << "rms = " << rms << std::endl;
         rms_avg += rms;
     }
     rms_avg /= jac_sqr_diff.cols();
 
-    BOOST_CHECK(rms_avg < 0.02);
+    std::cerr << "rms average = " << rms_avg << std::endl;
+
+    BOOST_CHECK(rms_avg < 3e-3);
 }
 
 BOOST_FIXTURE_TEST_SUITE(pca_rt, AtmosphereFixture)
