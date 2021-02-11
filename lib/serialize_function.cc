fea52e9b1721484fe425e431c98fdd258d6fd85a
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2021-02-10 20:52:40 -0800

    Fix bug found with geocal variations buildbot test.

--- lib/serialize_function.cc
+++ lib/serialize_function.cc
@@ -99,6 +99,7 @@ void SWIG_MAPPER_NAMESPACE::skip_weak_ptr_handling(bool Skip)
 
 static void mark_pointer(const boost::shared_ptr<GenericObject>& Obj)
 {
+#ifdef SWIG_HAVE_BOOST_SERIALIZATION
   if(skip_weak_ptr_handling())
     return;
   clear_ptr_serialized_reference();
@@ -106,6 +107,9 @@ static void mark_pointer(const boost::shared_ptr<GenericObject>& Obj)
   boost::archive::polymorphic_binary_oarchive oa(os_first_pass);
   oa << boost::serialization::make_nvp("geocal_object", Obj);
   // Don't actually want output, just side effect of marking pointers
+#else
+  throw std::runtime_error("SWIG_MAPPER_NAMESPACE was not built with boost::serialization support");
+#endif
 }
 
 //-----------------------------------------------------------------------
