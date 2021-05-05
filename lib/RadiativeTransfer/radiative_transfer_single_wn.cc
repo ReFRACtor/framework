7218b013d86e44838923ebd13b1e60b6e43bbcdf
James McDuffie <James.McDuffie@jpl.nasa.gov>
2021-05-05 10:15:12 -0700

    Fix progress bar and add timing logging output to RadiativeTransferSingleWn

--- lib/RadiativeTransfer/radiative_transfer_single_wn.cc
+++ lib/RadiativeTransfer/radiative_transfer_single_wn.cc
@@ -20,14 +20,20 @@ Array<double, 2>
 RadiativeTransferSingleWn::stokes(const SpectralDomain& Spec_domain,
 				  int Spec_index) const
 {
+  FunctionTimer ft(timer.function_timer(true));
+
   Array<double, 1> wn(Spec_domain.wavenumber());
   boost::shared_ptr<boost::progress_display> disp = progress_display(wn);
   Array<double, 2> res(wn.rows(), number_stokes());
+
   for(int i = 0; i < wn.rows(); ++i) {
     res(i, Range::all()) = stokes_single_wn(wn(i), Spec_index);
     if(disp)	
       *disp += 1;
   }
+
+  Logger::info() << atm->timer_info();
+
   return res;
 }
 
@@ -35,6 +41,8 @@ ArrayAd<double, 2>
 RadiativeTransferSingleWn::stokes_and_jacobian(const SpectralDomain& Spec_domain,
 					       int Spec_index) const
 {
+  FunctionTimer ft(timer.function_timer(true));
+
   Array<double, 1> wn(Spec_domain.wavenumber());
   if(wn.rows() < 1)		// Handle degenerate case.
     return ArrayAd<double, 2>(0,number_stokes(),0);
@@ -43,11 +51,18 @@ RadiativeTransferSingleWn::stokes_and_jacobian(const SpectralDomain& Spec_domain
     stokes_and_jacobian_single_wn(wn(0), Spec_index);
   ArrayAd<double, 2> res(wn.rows(), number_stokes(), t.number_variable());
   res(0, Range::all()) = t;
+
+  // Advance progress bar for first computation used to size the result
+  *disp += 1;
+
   for(int i = 1; i < wn.rows(); ++i) {
     res(i, Range::all()) = stokes_and_jacobian_single_wn(wn(i), Spec_index);
     if(disp)	
       *disp += 1;
   }
+
+  Logger::info() << atm->timer_info();
+
   return res;
 }
 
