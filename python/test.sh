524dc84fc8dcdb02625a29166f67bfb35966ca13
James McDuffie <James.McDuffie@jpl.nasa.gov>
2021-09-16 12:59:35 -0700

    Change Python test script to be callable from any directory and locate Python test directory.

--- python/test.sh
+++ python/test.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-PYTHONPATH=$(pwd):${PYTHONPATH} py.test -rxXs --log-cli-level=info test
+src_test_dir=$(dirname $0)/test
+
+PYTHONPATH=$(pwd):${PYTHONPATH} py.test -rxXs --log-cli-level=info $src_test_dir
