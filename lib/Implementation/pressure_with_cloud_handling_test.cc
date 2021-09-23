0b76f7d5ae4d5ce0000c23c0cda6053fbf6b0edd
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2021-09-22 18:12:04 -0700

    Have PressureWithCloudHandling working with a test for reverse pressure.

--- lib/Implementation/pressure_with_cloud_handling_test.cc
+++ lib/Implementation/pressure_with_cloud_handling_test.cc
@@ -24,18 +24,35 @@ BOOST_AUTO_TEST_CASE(basic)
   a = 0; b = 0.3, 0.6, 1.0;
   double psurf = 10;
   auto psigma = boost::make_shared<PressureSigma>(a, b, psurf);
+  auto psigma2 = boost::make_shared<PressureSigma>(a.reverse(blitz::firstDim),
+			   b.reverse(blitz::firstDim),
+			   psurf, Pressure::PREFER_DECREASING_PRESSURE);
   PressureWithCloudHandling p(psigma, 7);
+  PressureWithCloudHandling p2(psigma2, 7);
+  BOOST_CHECK_EQUAL(p.type_preference(), Pressure::PREFER_INCREASING_PRESSURE);
+  BOOST_CHECK_EQUAL(p2.type_preference(), Pressure::PREFER_DECREASING_PRESSURE);
   PrintNotify obs;
   p.add_observer(obs);
   Array<double, 1> press_grid_expect(3);
   press_grid_expect = 3, 6, 10;
+  Array<double, 1> press_grid_expect2(3);
+  press_grid_expect2 = 10, 6, 3;
   BOOST_CHECK_MATRIX_CLOSE(p.pressure_grid().value.value(), press_grid_expect);
+  BOOST_CHECK_MATRIX_CLOSE(p.pressure_grid(Pressure::NATIVE_ORDER).value.value(), press_grid_expect);
+  BOOST_CHECK_MATRIX_CLOSE(p2.pressure_grid().value.value(), press_grid_expect);
+  BOOST_CHECK_MATRIX_CLOSE(p2.pressure_grid(Pressure::NATIVE_ORDER).value.value(), press_grid_expect2);
   std::cerr << p.pressure_grid().value.value() << "\n";
   p.do_cloud(true);
+  p2.do_cloud(true);
   std::cerr << p.pressure_grid().value.value() << "\n";
-  Array<double, 1> press_grid_expect2(2);
-  press_grid_expect2 = 3, 6;
-  BOOST_CHECK_MATRIX_CLOSE(p.pressure_grid().value.value(), press_grid_expect2);
+  Array<double, 1> press_grid_expect3(2);
+  press_grid_expect3 = 3, 6;
+  Array<double, 1> press_grid_expect4(2);
+  press_grid_expect4 = 6, 3;
+  BOOST_CHECK_MATRIX_CLOSE(p.pressure_grid().value.value(), press_grid_expect3);
+  BOOST_CHECK_MATRIX_CLOSE(p.pressure_grid(Pressure::NATIVE_ORDER).value.value(), press_grid_expect3);
+  BOOST_CHECK_MATRIX_CLOSE(p2.pressure_grid().value.value(), press_grid_expect3);
+  BOOST_CHECK_MATRIX_CLOSE(p2.pressure_grid(Pressure::NATIVE_ORDER).value.value(), press_grid_expect4);
   psigma->set_surface_pressure(15);
 }
 
