149c925fecb8ccce3e260f14bbc24b1085e1e244
James McDuffie <James.McDuffie@jpl.nasa.gov>
2021-09-28 15:17:35 -0700

    Update blitz version to the latest from: https://github.com/blitzpp/blitz

--- thirdparty/blitz/blitz.cmake
+++ thirdparty/blitz/blitz.cmake
@@ -1,17 +1,15 @@
 set(BLITZ_NAME blitz)
 
 # Location of tar file to build
-set(BLITZ_URL ${CMAKE_CURRENT_SOURCE_DIR}/blitz/blitz-0.10.tar.gz)
+set(BLITZ_URL ${CMAKE_CURRENT_SOURCE_DIR}/blitz/blitz-39f8859.tar.gz)
 
-# Patch to fix build problem with gcc 7
-set(BLITZ_PATCH ${CMAKE_CURRENT_SOURCE_DIR}/blitz/blitz-fix-gcc7.patch)
+# Set up arguments to cmake call
+# 1. Use toolchain file to set cmake options that fix compilation issues
+# 2. Set the install path to the third party install path
+set(CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_SOURCE_DIR}/blitz/toolchain_file.cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} )
 
 ExternalProject_Add(${BLITZ_NAME}
     URL ${BLITZ_URL}
-    PATCH_COMMAND patch -p1 < ${BLITZ_PATCH}
-    CONFIGURE_COMMAND ./configure --prefix ${THIRDPARTY_INSTALL_PREFIX} --enable-shared --enable-shared --disable-doxygen
-        CXX=${CMAKE_CXX_COMPILER} CC=${CMAKE_C_COMPILER} F77=${CMAKE_Fortran_COMPILER}
+    CONFIGURE_COMMAND COMMAND ${CMAKE_COMMAND} ${CMAKE_ARGS} .
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND make 
-    INSTALL_COMMAND make install
 )
