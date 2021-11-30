7d6fc029a14371993afd616266711e97c502c15a
James McDuffie <James.McDuffie@jpl.nasa.gov>
2021-11-30 11:17:27 -0800

    Fix bug within LIDORT when delta-m scaling is turned off and number of streams equals 1. Add back 2stream RT unit tests that check both with delta m scaling turned on and off.

--- lib/RadiativeTransfer/twostream_rt_test.cc
+++ lib/RadiativeTransfer/twostream_rt_test.cc
@@ -13,6 +13,7 @@ void compare_lidort_2stream(boost::shared_ptr<RtAtmosphere>& atmosphere,
                             blitz::Array<double, 1>& zen, 
                             blitz::Array<double, 1>& azm, 
                             blitz::Array<double, 1>& wn_arr,
+                            bool do_deltam,
                             bool debug_output)
 { 
   int nstreams = 1;
@@ -20,20 +21,25 @@ void compare_lidort_2stream(boost::shared_ptr<RtAtmosphere>& atmosphere,
   bool do_multiple_scattering_only = true;
   bool pure_nadir = false;
 
+  // Set up LIDORT
   boost::shared_ptr<LidortRt> lidort_rt;
   lidort_rt.reset(new LidortRt(atmosphere, stokes_coefs, sza, zen, azm, pure_nadir,
                                nstreams, nmoms, do_multiple_scattering_only));
 
-  // Plane-parallel
   lidort_rt->rt_driver()->set_plane_parallel();
 
-  ArrayAd<double, 2> lid_rad_jac = lidort_rt->stokes_and_jacobian(wn_arr, 0);
+  lidort_rt->rt_driver()->lidort_interface()->lidort_modin().mbool().ts_do_deltam_scaling(do_deltam);
 
+  // Set up 2stream
   boost::shared_ptr<TwostreamRt> twostream_rt;
   twostream_rt.reset(new TwostreamRt(atmosphere, stokes_coefs, sza, zen, azm, false));
 
-  // Plane-parallel
   twostream_rt->rt_driver()->twostream_interface()->do_plane_parallel(true);
+
+  twostream_rt->rt_driver()->twostream_interface()->do_d2s_scaling(do_deltam);
+
+  // Make RT calls
+  ArrayAd<double, 2> lid_rad_jac = lidort_rt->stokes_and_jacobian(wn_arr, 0);
 
   ArrayAd<double, 2> ts_rad_jac = twostream_rt->stokes_and_jacobian(wn_arr, 0);
 
@@ -65,7 +71,11 @@ BOOST_FIXTURE_TEST_SUITE(twostream_rt_lambertian, LidortLambertianFixture)
 
 BOOST_AUTO_TEST_CASE(comparison)
 { 
-  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, false);
+  // Delta-m on
+  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, false, false);
+
+  // Delta-m off
+  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, true, false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -75,7 +85,11 @@ BOOST_FIXTURE_TEST_SUITE(twostream_rt_coxmunk, LidortCoxmunkFixture)
 
 BOOST_AUTO_TEST_CASE(comparison)
 { 
-  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, false);
+  // Delta-m on
+  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, true, false);
+
+  // Delta-m off
+  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, false, false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -85,7 +99,11 @@ BOOST_FIXTURE_TEST_SUITE(twostream_rt_coxmunk_plus_lambertian, LidortCoxmunkPlus
 
 BOOST_AUTO_TEST_CASE(comparison)
 { 
-  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, false);
+  // Delta-m on
+  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, true, false);
+
+  // Delta-m off
+  compare_lidort_2stream(config_atmosphere, stokes_coefs, config_state_vector, sza, zen, azm, wn_arr, false, false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
