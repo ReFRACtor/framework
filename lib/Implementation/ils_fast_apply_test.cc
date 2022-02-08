6e6d87a830edd1319c6fcbc1b59b9ac14e1c0c3c
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-02-08 12:55:09 -0800

    Rewrite IlsFastApply unit test to use data collected from an OMI MUSES case

--- lib/Implementation/ils_fast_apply_test.cc
+++ lib/Implementation/ils_fast_apply_test.cc
@@ -1,58 +1,63 @@
 #include "ils_fast_apply.h"
-#include "hdf_file.h"
-#include "dispersion_polynomial.h"
 #include "unit_test_support.h"
+#include "hdf_file.h"
+#include "sample_grid_spectral_domain.h"
 
 using namespace FullPhysics;
 using namespace blitz;
 
 BOOST_FIXTURE_TEST_SUITE(ils_fast_apply, GlobalFixture)
 
-BOOST_AUTO_TEST_CASE(basic)
+BOOST_AUTO_TEST_CASE(omi_muses_case)
 {
-    int num_pixels = 1016;
+    // Extract expected values and inputs
+    HdfFile inp_vals(test_data_dir() + "expected/ils_fast_apply/omi_muses_case.h5");
+  
+    Array<double, 2> svh_isrf_fft_real = inp_vals.read_field<double, 2>("ILS_Parameters/svh_isrf_fft_real");
+    Array<double, 2> svh_isrf_fft_imag = inp_vals.read_field<double, 2>("ILS_Parameters/svh_isrf_fft_imag");
+    Array<double, 2> scaled_uh_isrf = inp_vals.read_field<double, 2>("ILS_Parameters/scaled_uh_isrf");
+    Array<int, 1> extract_indices = inp_vals.read_field<int, 1>("ILS_Parameters/extract_indices");
 
+    Array<double, 1> mono_grid = inp_vals.read_field<double, 1>("Monochromatic/grid");
+    Array<double, 1> mono_rad = inp_vals.read_field<double, 1>("Monochromatic/radiance");
+    Array<double, 2> mono_jac = inp_vals.read_field<double, 2>("Monochromatic/jacobian");
+
+    Array<double, 1> conv_expt_grid = inp_vals.read_field<double, 1>("Convolved/grid");
+    Array<double, 1> conv_expt_rad = inp_vals.read_field<double, 1>("Convolved/radiance");
+    Array<double, 2> conv_expt_jac = inp_vals.read_field<double, 2>("Convolved/jacobian");
+
+    // Some hard coded values
+    std::string band_name = "Band_1";
+    DoubleWithUnit high_res_ext = DoubleWithUnit(0.315, units::nm);
+
+    // Set up SampleGrid object
+    SpectralDomain conv_sd(conv_expt_grid, units::nm);
+    boost::shared_ptr<SampleGridSpectralDomain> samp_grid(new SampleGridSpectralDomain(conv_sd, band_name));
+
+    // Create IlsFastApply object
+    IlsFastApply ils = IlsFastApply(scaled_uh_isrf, svh_isrf_fft_real, svh_isrf_fft_imag, extract_indices, samp_grid, high_res_ext, band_name, band_name);
+
+    // Create a pixel list that references all samples on the convolved grid
     std::vector<int> pixel_list;
-    for(int i = 0; i < num_pixels; i++) {
+    for(int i = 0; i < conv_expt_grid.rows(); i++) {
         pixel_list.push_back(i);
     }
 
-    Array<bool, 1> flag(6);
-    flag = false;
-  
-    Array<double, 1> coeff(6);
-    coeff =  7.57646001e-01,   1.74935138e-05,  -2.73614810e-09,
-        -7.51159779e-14,   1.32166257e-16,  -7.27469045e-20;
-  
-    boost::shared_ptr<DispersionPolynomial>
-      dispersion(new DispersionPolynomial(coeff, flag, units::micron, "A-Band", num_pixels, true));
-  
-    HdfFile inp_vals(test_data_dir() + "expected/ils_fast_apply/fft_svd_ils_values.h5");
-  
-    auto scaled_uh_isrf = inp_vals.read_field<double, 2>("scaled_uh_isrf");
-    auto svh_isrf_fft_real = inp_vals.read_field<double, 2>("svh_isrf_fft_real");
-    auto svh_isrf_fft_imag = inp_vals.read_field<double, 2>("svh_isrf_fft_imag");
-    auto extract_indices = inp_vals.read_field<int, 1>("center_freq_indices");
-  
-    auto ils_half_width = DoubleWithUnit(4.09e-04, "um");
-  
-    auto ils = IlsFastApply(scaled_uh_isrf,
-                         svh_isrf_fft_real,
-                         svh_isrf_fft_imag,
-                         extract_indices, 
-                         dispersion, ils_half_width, "A-Band", "abo2");
+    // Convert mono grid units to wavenumbers to meet interface requirements
+    Array<double, 1> mono_wn = ArrayWithUnit<double, 1>(mono_grid, units::nm).convert_wave(units::inv_cm).value;
 
-    auto high_res_grid = inp_vals.read_field<double, 1>("high_res_grid");
-    auto high_res_radiance = inp_vals.read_field<double, 1>("high_res_radiance");
+    // Apply ILS convolution for no jacobian case
+    Array<double, 1> conv_calc_rad = ils.apply_ils(mono_wn, mono_rad, pixel_list);
 
-    ArrayAd<double, 1> high_res_rad_ad(high_res_radiance.rows(), 0);
-    high_res_rad_ad.value() = high_res_radiance;
+    BOOST_CHECK_MATRIX_CLOSE_TOL(conv_expt_rad, conv_calc_rad, 1e-10);
 
-    auto calc_conv_rad = ils.apply_ils(high_res_grid, high_res_rad_ad, pixel_list);
+    // Apply ILS convolution for jacobian case
+    ArrayAd<double, 1> mono_rad_jac(mono_rad, mono_jac);
 
-    auto expected_conv_rad = inp_vals.read_field<double, 1>("expected_convolved_radiance");
+    ArrayAd<double, 1> conv_calc_rad_jac = ils.apply_ils(mono_wn, mono_rad_jac, pixel_list);
 
-    BOOST_CHECK_MATRIX_CLOSE_TOL(expected_conv_rad, calc_conv_rad.value(), 1e-7);
+    BOOST_CHECK_MATRIX_CLOSE_TOL(conv_expt_rad, conv_calc_rad_jac.value(), 1e-10);
+    BOOST_CHECK_MATRIX_CLOSE_TOL(conv_expt_jac, conv_calc_rad_jac.jacobian(), 1e-10);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
