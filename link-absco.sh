4fff37196c3bc365fae15e4bc7b88453294920b2
Valentin Kantchev <valentin.h.kantchev@jpl.nasa.gov>
2022-08-24 11:10:18 -0700

    Update link-absco.sh

--- link-absco.sh
+++ link-absco.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-if [ -d ~/refractor/absco ] 
+if [ -d ~/refractor-dev/absco ] 
 then
-    echo "Found ~/refractor/absco. Creating a link at ./absco" 
-    ln -snf ~/refractor/absco absco
+    echo "Found ~/refractor-dev/absco. Creating a link at ./absco" 
+    ln -snf ~/refractor-dev/absco absco
 else
-    echo "Directory ~/refractor/absco does not exists. This is not critical. However you won't be able to run / debug the Python tests."
+    if [ -d ~/refractor/absco ] 
+    then
+        echo "Found ~/refractor/absco ... Creating a link at ./absco" 
+        ln -snf ~/refractor/absco absco
+    else
+        echo "Directories ~/refractor-dev/absco or ~/refractor/absco do not exist. \
+              This is not critical. However you won't be able to run / debug the Python tests."
+    fi
+
 fi
