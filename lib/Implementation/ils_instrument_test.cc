43b2296f2f8330ab3fcf5d2d096c65a1a6b987c6
Sebastian Val <sval@jpl.nasa.gov>
2023-09-19 12:54:22 -0700

    Adding unit test that shows issue in ILS application with differing units for ILS sample grid and high resolution window.

--- lib/Implementation/ils_instrument_test.cc
+++ lib/Implementation/ils_instrument_test.cc
@@ -142,6 +142,47 @@ BOOST_AUTO_TEST_CASE(basic_static_test)
 }
 BOOST_AUTO_TEST_SUITE_END()
 
+BOOST_FIXTURE_TEST_SUITE(ils_instrument_diff_static, LambertianConfigurationFixture)
+
+BOOST_AUTO_TEST_CASE(basic_static_different_units_test)
+{
+  HdfFile hf(test_data_dir() + "in/ils/ils_linear_table.h5");
+  boost::shared_ptr<IlsTableLinear> ils_tab(new IlsTableLinear(hf, 0, "A-Band", "o2"));
+  IfstreamCs sd_in_data(test_data_dir() + "expected/dispersion_polynomial/gosat");
+  Array<double, 1> pwn_in;
+  sd_in_data >> pwn_in;
+  SpectralDomain sd_in(pwn_in, units::inv_cm);
+  boost::shared_ptr<SampleGridSpectralDomain>
+    sg(new SampleGridSpectralDomain(sd_in, ils_tab->band_name()));
+  std::vector<boost::shared_ptr<Ils> > ils;
+  ils.push_back(boost::shared_ptr<Ils>(new IlsGrating(sg, ils_tab)));
+
+  IlsInstrument inst(ils);
+
+  std::vector<int> plist;
+  // Arbitrary list of pixels.
+  plist.push_back(403);
+  plist.push_back(405);
+  for(int i = 407; i <= 414; ++i)
+    plist.push_back(i);
+
+  IfstreamCs expected(test_data_dir() + "expected/ils_grating/basic");
+  Array<double, 1> wn_in, rad_hres_in, rad_out_expect;
+  expected >> wn_in >> rad_hres_in >> rad_out_expect;
+  SpectralDomain sd(wn_in, units::inv_cm);
+  Array<double, 1> sd_convert_data(wn_in.rows());
+  sd_convert_data = sd.convert_wave(units::micron).reverse(firstDim)(Range::all());
+  SpectralDomain sd_micron(sd_convert_data, units::micron);
+
+  Array<double, 1> sr_convert_data(rad_hres_in.rows());
+  sr_convert_data = rad_hres_in.reverse(firstDim)(Range::all());
+  SpectralRange sr(sr_convert_data, units::dimensionless); // Ignore units
+  BOOST_CHECK_MATRIX_CLOSE(inst.apply_instrument_model(Spectrum(sd_micron, sr),
+                                                       plist, 0).
+                           spectral_range().data(), rad_out_expect);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_FIXTURE_TEST_SUITE(ils_instrument_timing, LambertianConfigurationFixture)
 
 BOOST_AUTO_TEST_CASE(timing)
