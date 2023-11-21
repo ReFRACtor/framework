0ce97427af5693ad4a6e10f512e5c32f6d0b5c56
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2023-11-21 15:50:57 -0800

    Checkpoint work, trying to get print override working, but save what we have.

--- lib/serialize_function.cc
+++ lib/serialize_function.cc
@@ -2,6 +2,7 @@
 #include "weak_ptr_serialize_support.h"
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/regex.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 #include <stdexcept>
 #include <fstream>
@@ -19,6 +20,74 @@
 #include <boost/archive/polymorphic_binary_oarchive.hpp>
 #endif
 using namespace SWIG_MAPPER_NAMESPACE;
+
+/****************************************************************//**
+   This allows filtering stream to created on top of an 
+   underlying ofstream. See boost::iostream documentation for details
+   on how to create these. This class is intended to be used as a base
+   class for other classes that assemble the proper filters.
+*******************************************************************/
+
+class FilteringOstream : public std::ostream {
+public:
+  virtual ~FilteringOstream() {}
+protected:
+
+//-----------------------------------------------------------------------
+/// Constructor. This opens the given file and sets up the stream
+/// buffer, but nothing is placed in the filter. The derived class should
+/// set up the streambuf.
+//-----------------------------------------------------------------------
+
+  FilteringOstream(const std::string& Fname) 
+    : std::ostream(&sb), f(Fname.c_str()) {}
+
+//-----------------------------------------------------------------------
+/// Underlying ofstream.
+//-----------------------------------------------------------------------
+
+  std::ofstream f;
+
+//-----------------------------------------------------------------------
+/// Filtering streambuf.
+//-----------------------------------------------------------------------
+
+  boost::iostreams::filtering_ostreambuf sb;
+};
+
+/****************************************************************//**
+   This allows filtering stream to created on top of an 
+   underlying ifstream. See boost::iostream documentation for details
+   on how to create these. This class is intended to be used as a base
+   class for other classes that assemble the proper filters.
+*******************************************************************/
+
+class FilteringIstream : public std::istream {
+public:
+  virtual ~FilteringIstream() {}
+protected:
+
+//-----------------------------------------------------------------------
+/// Constructor. This opens the given file and sets up the stream
+/// buffer, but nothing is placed in the filter. The derived class should
+/// set up the streambuf.
+//-----------------------------------------------------------------------
+
+  FilteringIstream(const std::string& Fname) 
+    : std::istream(&sb), f(Fname.c_str()) {}
+
+//-----------------------------------------------------------------------
+/// Underlying ifstream.
+//-----------------------------------------------------------------------
+
+  std::ifstream f;
+
+//-----------------------------------------------------------------------
+/// Filtering streambuf.
+//-----------------------------------------------------------------------
+
+  boost::iostreams::filtering_istreambuf sb;
+};
 
 /****************************************************************//**
 /// This opens a file for writing, possibly with automatic
